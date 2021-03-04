73c9743f7cfb2478a15fa2fc25087c973317895c
leila571 <leilamarks@postoakschool.org>
2021-03-04 12:49:20 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+"""
+Leila Lopez Marks
+3/4/21
+Pseudocode Conversion Homework
+"""
+
+//==== Sequential Search =========
+Numbers = [2/ 9, 5, 6, 7, 8]
+x = 7
+Found=false
+Counter = 0
+loop Counter()
+from 0 to 5
+    if Number[Counter] = X then
+Found = true
+output Number[Counter],"found at position" , Counter
+loop()
+
+if Found = false then
+output X,"not found"
+if()
+
+# Binary
+Search == == =
+VALUES = [11, 12, 15, 16, 112, 118, 123, 145] 
+TARGET = 15 // search
+#value
+MIN = 0
+HIGH = 7 // Number
+
+FOUND = false
+ANSWER = 0
+MID = 0
+def number_statements():
+    while FOUND = true AND MIN <= HIGH
+ MID = ((MIN + HIGH) div 2)
+    if VALUES[MID] = TARGET then
+FOUND = true
+ ANSWER = MID
+    else if TARGET > VALUES[MID] then
+ MIN = MID + 1
+    else, HIGH = MID — 1
+number_statements()
+
+def true_statement():
+    if FOUND = true
+        print("statement was found to be true")
+    else FOUND = false
+        print("statement was found to be false")
+
+
+true_statement()
